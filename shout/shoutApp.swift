a6651005f0615fae6cb6636b3425d8f3b85f7865
RokwonK <wonrok97@naver.com>
2023-04-23 15:38:38 +0900

    🎉 initial

---
+++ shout/shoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoutApp.swift
+//  shout
+//
+//  Created by 김록원 on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct shoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
