27d58c9592591e0164a90b59a597f8f74fafadeb
RokwonK <wonrok97@naver.com>
2023-04-23 23:00:25 +0900

    ✨ Image 및 Color 추가

---
+++ shout/Common/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  shout
+//
+//  Created by 김록원 on 2023/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+  
+  static let dark500 = Color("dark500")
+  static let dark700 = Color("dark700")
+  
+  
+  static let blue500 = Color("blue500")
+  static let orange500 = Color("orange500")
+  static let green500 = Color("green500")
+  static let purple500 = Color("purple500")
+}
