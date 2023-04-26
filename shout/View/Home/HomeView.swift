be6c84e677afe8c4ea06f8782b6413e75b8515a6
RokwonK <wonrok97@naver.com>
2023-04-26 18:18:49 +0900

    🎨 HomeView 구조 및 네비게이션 추가

--- shout/View/Home/HomeView.swift
+++ shout/View/Home/HomeView.swift
@@ -6,30 +6,76 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct HomeView: View {
-    var body: some View {
-      ScrollView{
-        LazyVStack{
-          Text("Home")
-            .frame(height: 300)
-          Text("Home")
-            .frame(height: 300)
-          Text("Home")
-            .frame(height: 300)
-          Text("Home")
-            .frame(height: 300)
-          Text("Home")
-            .frame(height: 300)
-          Text("Home")
-            .frame(height: 300)
+  let store: StoreOf<HomeCore>
+  let mainListName = "HomeViewMainList"
+  
+  var body: some View {
+    WithViewStore(self.store, observe: { $0 }) { viewStore in
+      ScrollView {
+        LazyVStack {
+          ForEach(0..<10) {_ in
+            Text("Hello World")
+              .frame(height: 300)
+              .listRowSeparator(.hidden)
+              .listRowInsets(.none)
+              .listRowBackground(Color.dark700)
+          }
+        }
+        .background(GeometryReader { proxy in
+          Color.clear
+            .preference(
+              key: ScrollOffsetKey.self,
+              value: -proxy.frame(in: .named(mainListName)).origin.y
+            )
+          }
+        )
+      }
+      .listStyle(.plain)
+      .scrollContentBackground(.hidden)
+      .coordinateSpace(name: mainListName)
+      .onPreferenceChange(ScrollOffsetKey.self) { offsetY in
+        viewStore.send(.mainListScrolled(offsetY))
+      }
+      .safeAreaInset(edge: .top) {
+        NavigationBar(isTransparent: viewStore.isHeaderTransParent) {
+          HStack(spacing: 8) {
+            Image("brand")
+              .renderingMode(.template)
+              .resizable()
+              .scaledToFit()
+              .frame(height: 20)
+              .foregroundColor(.white)
+            Text("외침")
+              .font(.system(size: 20))
+              .fontWeight(.heavy)
+          }
         }
       }
     }
+  }
 }
 
+
+// MARK: Scroll Event
+fileprivate struct ScrollOffsetKey: PreferenceKey {
+  static var defaultValue: CGFloat = .zero
+  static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+    value += nextValue()
+  }
+}
+
+
+// MARK: Preview
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+  static var previews: some View {
+    HomeView(store: Store(
+      initialState: HomeCore.State(),
+      reducer: HomeCore()
+    ))
+      .preferredColorScheme(.dark)
+      .background(Color.dark700)
+  }
 }
