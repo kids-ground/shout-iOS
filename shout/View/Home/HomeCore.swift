1b1a62bee105116baf1079a85377ab6ad1cc11ea
RokwonK <wonrok97@naver.com>
2023-04-26 18:18:23 +0900

    ✨ HomeCore 구현

---
+++ shout/View/Home/HomeCore.swift
@@ -0,0 +1,36 @@
+//
+//  HomeCore.swift
+//  shout
+//
+//  Created by 김록원 on 2023/04/26.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct HomeCore: ReducerProtocol {
+  struct State: Equatable {
+    var scrollOffsetY: CGFloat = 0.0
+    var isHeaderTransParent = false
+  }
+  
+  enum Action: Equatable {
+    case mainListScrolled(CGFloat)
+    case scrollDebounceCompleted(CGFloat)
+  }
+  
+  struct DebounceId: Hashable {}
+  
+  func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+    switch action {
+    case .mainListScrolled(let offsetY):
+      return .task { .scrollDebounceCompleted(offsetY) }
+      .throttle(id: DebounceId(), for: 0.3, scheduler: DispatchQueue.main, latest: true)
+    case .scrollDebounceCompleted(let offsetY):
+      if state.isHeaderTransParent != (offsetY >= 10) {
+        state.isHeaderTransParent.toggle()
+      }
+      return .none
+    }
+  }
+}
