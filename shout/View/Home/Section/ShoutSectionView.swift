84f5fe41a0d81977564225318f31c66f0505d935
RokwonK <wonrok97@naver.com>
2023-04-27 18:46:54 +0900

    ✨ 홈뷰 외침 섹션 구현

---
+++ shout/View/Home/Section/ShoutSectionView.swift
@@ -0,0 +1,27 @@
+//
+//  ShoutSectionView.swift
+//  shout
+//
+//  Created by 김록원 on 2023/04/27.
+//
+
+import SwiftUI
+
+struct ShoutSectionView: View {
+    var body: some View {
+      ScrollView(.horizontal, showsIndicators: false) {
+        LazyHStack(spacing: 16) {
+          ForEach(0..<10) { data in
+            ShoutSimpleCell()
+          }
+        }
+        .padding(.horizontal, 24)
+      }
+    }
+}
+
+struct ShoutSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoutSectionView()
+    }
+}
