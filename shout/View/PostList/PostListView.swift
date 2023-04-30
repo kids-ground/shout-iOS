9b7d549408748527e5acbb78cfed1deb305d9dd9
RokwonK <wonrok97@naver.com>
2023-04-30 15:44:21 +0900

    ✨ PostListView 뷰 구현 완료

--- shout/View/PostList/PostListView.swift
+++ shout/View/PostList/PostListView.swift
@@ -50,23 +50,26 @@ struct PostListView: View {
       
       TabView(selection: $pageIdx) {
         ScrollView{
-          LazyVStack{
+          LazyVStack(spacing: 24){
             ForEach(0..<7) { idx in
-              Text("Shout \(idx)")
-                .frame(height: 300)
+              ShoutInteractiveCell()
             }
           }
+          .padding(24)
+          .padding(.bottom, TabBarView.originalTabBarHeight)
         }
         .tag(0)
         .scrollIndicators(.hidden)
         
+        
         ScrollView{
-          LazyVStack{
+          LazyVStack(spacing: 24){
             ForEach(0..<7) { idx in
-              Text("WiseSaying \(idx)")
-                .frame(height: 300)
+              WiseSayingInteractiveCell()
             }
           }
+          .padding(24)
+          .padding(.bottom, TabBarView.originalTabBarHeight)
         }
         .tag(1)
         .scrollIndicators(.hidden)
@@ -83,8 +86,117 @@ struct PostListView: View {
         }
       }
     }
+    .edgesIgnoringSafeArea(.bottom)
   }
 }
+
+
+struct ShoutInteractiveCell: View {
+  @State var isActive = [false, false]
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 16) {
+      HStack(spacing: 12) {
+        AsyncImage(url: URL(string:"https://hws.dev/paul.jpg")) { image in
+          image.resizable()
+            .frame(width: 35, height: 35)
+            .scaledToFit()
+            .clipShape(Circle())
+        } placeholder: {
+          VStack{ }.frame(width: 35, height: 35)
+        }
+        Text("빌게이츠")
+          .font(.system(size: 14))
+          .fontWeight(.bold)
+      }
+      Text("세상에 돈보다 중요한게 있다고? 그건 말이 안되는 소리입니다. 여러분, 사탕발림소리에 속지마세요. 돈 머니가 최곱니다 여러분! 저를 보세요 이거리얼입니다! 사탕발림소리에 속지마세요. 돈 머니가 최곱니다 여러분! 저를 보세요 이거리얼입니다! 사탕발림소리에 속지마세요. 돈 머니가 최곱니다 여러분! 저를 보세요 이거리얼입니다! ")
+        .font(.system(size: 14))
+        .multilineTextAlignment(.leading)
+        .lineSpacing(4)
+        .lineLimit(nil)
+      
+      Spacer().frame(height: 0.1)
+      
+      HStack(alignment: .bottom, spacing: 8) {
+        ReactionButton(imageName: "heart", isActive: isActive[0], count: 123, activeTint: .pink, inActiveTint: .gray) { isActive in
+          self.isActive[0] = isActive
+        }
+        
+        ReactionButton(imageName: "star", isActive: isActive[1], count: 123, activeTint: .yellow, inActiveTint: .gray) { isActive in
+          self.isActive[1] = isActive
+        }
+      }
+      .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+    }
+    .padding(16)
+    .frame(maxWidth: .infinity, maxHeight: .infinity , alignment: .topLeading)
+    .background(Color.dark500)
+    .cornerRadius(16)
+  }
+}
+
+
+struct WiseSayingInteractiveCell: View {
+  @State var isActive = [false, false]
+  
+  var body: some View {
+    VStack(spacing: 12) {
+      ZStack(alignment: .center) {
+        Color.colors[Int.random(in: 0..<12)]
+          .frame(width: 100, height:200)
+          .blur(radius: 100)
+        VStack(spacing: 24) {
+          VStack(alignment: .center, spacing: 12) {
+            Image(systemName: "quote.opening")
+              .imageScale(.small)
+            Text("멈추면 풍경을 만나고 걸으면 추억을 만나며 달리면 꿈을 만난다.\n 풍경을 볼 것인가? 꿈을 향해 달릴 것인가? 선택은 당신의 몫이다. 무엇을 선택하든 당신의 정신과 육체가 한 뜻을 이루길 바란다.")
+              .font(.system(size: 15))
+              .fontWeight(.semibold)
+              .multilineTextAlignment(.center)
+              .lineSpacing(4)
+            Image(systemName: "quote.closing")
+              .imageScale(.small)
+          }
+          .frame(maxWidth: .infinity, alignment: .center)
+          
+          VStack(alignment: .center, spacing: 6) {
+            AsyncImage(url: URL(string:"https://hws.dev/paul.jpg")) { image in
+              image.resizable()
+                .frame(width: 30, height: 30)
+                .scaledToFit()
+                .clipShape(Circle())
+            } placeholder: {
+              VStack{ }.frame(width: 30, height: 30)
+            }
+            Text("Bill Gates")
+              .font(.system(size: 12))
+              .italic()
+          }
+        }
+      }
+      
+      Spacer()
+      
+      HStack(alignment: .bottom, spacing: 8) {
+        ReactionButton(imageName: "heart", isActive: isActive[0], count: 123, activeTint: .pink, inActiveTint: .gray) { isActive in
+          self.isActive[0] = isActive
+        }
+        
+        ReactionButton(imageName: "star", isActive: isActive[1], count: 123, activeTint: .yellow, inActiveTint: .gray) { isActive in
+          self.isActive[1] = isActive
+        }
+      }
+      .frame(maxWidth: .infinity, alignment: .trailing)
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+    .padding(.vertical, 24)
+    .padding(.horizontal, 16)
+    .background(Color.dark500)
+    .cornerRadius(16)
+  }
+}
+
+
 
 struct PostListView_Previews: PreviewProvider {
     static var previews: some View {
