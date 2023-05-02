7e061511fd1f427a0baa14fa400e89db53b9733b
RokwonK <wonrok97@naver.com>
2023-05-02 13:17:07 +0900

    ✨ Login에 필요한 Color, Image 추가

---
+++ shout/View/Page/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  shout
+//
+//  Created by 김록원 on 2023/05/02.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
