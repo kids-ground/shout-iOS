4267c961bbc2f71bdedab5bbb3bc03682f580cb4
RokwonK <wonrok97@naver.com>
2023-07-16 21:41:22 +0900

    ✨ LoginCore Actino 정의 및 구현
    ⚡️ 성능 개선
    🔥 코드 / 파일 삭제
    🐛 버그 수정
    🚑 긴급 수정
    ✨ 새 기능
    📝 문서 추가 / 수정
    💄 UI, 스타일 추가 / 수정
    🎉 프로젝트 시작
    ✅ 테스트 추가 / 수정
    🔒 보안 이슈 수정
    🔖 릴리즈 / 버전 태그
    💚 CI 빌드 수정
    📌 특정 버전 의존성 고정
    👷 CI 빌드 시스템 추가 / 수정
    📈 분석, 추가 코드 추가 / 수정
    ♻️ 코드 리팩토링
    ➕ 의존성 추가
    ➖ 의존성 제거
    🔧 구성 파일 추가 / 삭제
    🔨 개발 스크립트 추가 / 수정
    🌐 국제화 / 현지화
    💩 똥싼 코드
    ⏪ 변경 내용 되돌리기
    🔀 브랜치 합병
    📦 컴파일된 파일 추가 / 수정
    👽 외부 API 변화로 인한 수정
    🚚 리소스 이동, 이름 변경
    📄 라이센스 추가 / 수정
    💡 주석 추가 / 수정
    🍻 술 취해서 쓴 코드
    🗃 데이터베이스 관련 수정
    🔊 로그 추가 / 수정
    🙈 .gitignore 추가 / 수정

--- shout/View/Page/Login/LoginCore.swift
+++ shout/View/Page/Login/LoginCore.swift
@@ -10,6 +10,7 @@ import ComposableArchitecture
 import KakaoSDKAuth
 import KakaoSDKUser
 import AuthenticationServices
+import Moya
 
 struct LoginCore: ReducerProtocol {
   struct State: Equatable {
@@ -17,40 +18,74 @@ struct LoginCore: ReducerProtocol {
   }
   
   enum Action: Equatable {
+    static func == (lhs: LoginCore.Action, rhs: LoginCore.Action) -> Bool {
+      false
+    }
+    
     case kakaoLoginTap
-    case successKakaoLogin(String)
-    case failureKakaoLogin
-    case successAppleLogin(String)
-    case failureAppleLogin
+    case failureKakaoLogin(message: String)
+    case failureAppleLogin(message: String)
+    
+    case fetchSignUp(oAuthType: OAuthType, token: String)
+    case successSignUp(response: MemberReponseDto)
+    case failureSignUp(message: String)
   }
+
+
+  let userService = MoyaProvider<UserService>(plugins: [NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))])
+  
   
   func reduce(into state: inout State, action: Action) -> ComposableArchitecture.EffectTask<Action> {
     switch action {
     case .kakaoLoginTap:
-      if (UserApi.isKakaoTalkLoginAvailable()) {
-        UserApi.shared.loginWithKakaoTalk{ oauthToken, err in
-          if let err = err {
-            print(err)
-          } else {
-            print("카카오 로그인 \(oauthToken?.accessToken ?? "실패")")
+      return .task(priority: .userInitiated) {
+        let kakaoAccessToken = await withCheckedContinuation { continuation in
+          DispatchQueue.main.async {
+            if (UserApi.isKakaoTalkLoginAvailable()) {
+              UserApi.shared.loginWithKakaoTalk { oauthToken, err in
+                continuation.resume(returning: oauthToken?.accessToken)
+              }
+            } else {
+              UserApi.shared.loginWithKakaoAccount { oauthToken, err in
+                continuation.resume(returning: oauthToken?.accessToken)
+              }
+            }
           }
         }
-      } else {
-        UserApi.shared.loginWithKakaoAccount{ oauthToken, err in
-          if let err = err {
-            print(err)
-          } else {
-            print("카카오 로그인 \(oauthToken?.accessToken ?? "실패")")
-          }
-        }
+        
+        guard let token = kakaoAccessToken else { return .failureKakaoLogin(message: "토큰 없음") }
+        return .fetchSignUp(oAuthType: .KAKAO, token: token)
       }
+    case let .fetchSignUp(oAuthType, token):
+      return .task {
+        let request = LoginRequestDto(token: token, oAuthType: oAuthType)
+        let result = await userService.request(.login(requst: request))
+
+        switch result {
+        case .failure(let err):
+          print(err)
+          return .failureSignUp(message: "SignUp API 실패")
+        case .success(let response):
+          guard let responseData = try? response.map(MemberReponseDto.self) else { return .failureSignUp(message: "DTO 직렬화 실패")}
+          return .successSignUp(response: responseData)
+        }
+      } catch: { (err) in
+        return .failureSignUp(message: "통신 실패")
+      }
+    case .failureAppleLogin(let message):
+      print("애플 로그인 에러 발생 \(message)")
       return .none
-    case let .successKakaoLogin(accessToken):
+    case .failureKakaoLogin(let message):
+      print("카카오 로그인 에러 발생 \(message)")
       return .none
-    case let .successAppleLogin(identityToken):      
+    case .failureSignUp(let message):
+      print(message)
       return .none
-    case .failureAppleLogin, .failureKakaoLogin:
+    case let .successSignUp(response):
+      print("SignUP API 성공")
+      print(response.token.accessToken)
       return .none
     }
+    
   }
 }
