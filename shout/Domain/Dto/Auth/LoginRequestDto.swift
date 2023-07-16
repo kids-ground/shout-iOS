0998c57df156aebe83c302382431d3c1d89b1b3a
RokwonK <wonrok97@naver.com>
2023-07-16 21:39:44 +0900

    ✨ LoginRequestDto 추가

---
+++ shout/Domain/Dto/Auth/LoginRequestDto.swift
@@ -0,0 +1,18 @@
+//
+//  LoginRequestDto.swift
+//  shout
+//
+//  Created by 김록원 on 2023/07/15.
+//
+
+import Foundation
+
+enum OAuthType: String, Codable {
+  case KAKAO
+  case APPLE
+}
+
+struct LoginRequestDto: Encodable {
+  let token: String
+  let oAuthType: OAuthType
+}
